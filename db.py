522335045ae99adda845fa049722c7a35e7d8533
Shrayas <shrayasr@gmail.com>
2013-09-08 21:44:30 +0530

    query returns more meaningful results now

--- db.py
+++ db.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import g
 import sqlite3
+import json
 
 app = Flask(__name__)
 DB = 'test.db'
@@ -22,6 +23,10 @@ def query(query,args=(),one=False):
     rv = [dict((cur.description[index][0],value) for index, value in enumerate(row)) for row in cur.fetchall()]
     return (rv[0] if rv else None) if one else rv
 
+def insert(query):
+    g.db.execute(query)
+    g.db.commit()
+
 @app.route("/")
 def hello():
 	return "Hello world"
@@ -29,9 +34,8 @@ def hello():
 @app.route("/query")
 def makeSomeQuery():
     print query('select * from counts')
-    for counts in query('select * from counts'):
-        print str(counts['key']) + " ... " + str(counts['value'])
-    return "Check cmdline"
+    counts = query('select * from counts')
+    return json.dumps(counts)
 	
 if __name__ == "__main__":
     app.debug = True
